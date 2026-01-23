504badb79f00ec123f3d6b64b7ad6b16661ab5ae
fernandaUTZMG <fernanda.hernandez.22s@utzmg.edu.mx>
2026-01-23 08:24:19 -0600

    Resuelto conflicto en main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,12 @@ async function bootstrap() {
   // Escucha el puerto 3001 (o el que tengas en env)
   await app.listen(process.env.PORT ?? 3001);
 
-<<<<<<< HEAD
+
   console.log(`Servidor corriendo en http://localhost:${process.env.PORT ?? 3001}`);
-=======
+
   //holaaaaa hkglkjgk
   
->>>>>>> a52e03a2c5da9836c144dc0116cd8e5692c82716
+
 }
 
 bootstrap();
